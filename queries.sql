f1749531ae255f98b023b3caa7253bc066fef3ac
Dave <8709533+DaveBathnes@users.noreply.github.com>
2025-07-17 21:37:10 +0100

    Adding most popular author by postcode district

--- queries.sql
+++ queries.sql
@@ -10,6 +10,45 @@ select lsoa21cd from lsoa_lookup where lad24nm = 'Manchester'
 group by lsoa;
 
 
+-- select count of loans by postcode district
+select
+    distinct on (l.postcode_district),
+    count(*) as loan_count
+from loans l
+group by l.postcode_district
+order by l.postcode_district, loan_count desc;
+
+-- select most popular author by postcode district
+-- only include manchester postcode districts (starting with M and then a number)
+select 
+    distinct on (postcode_district)
+    postcode_district,
+    author, 
+    count(*) as loan_count
+from loans
+where postcode_district is not null
+and postcode_district ~ '^M[0-9]'
+and author is not null
+group by postcode_district, author  
+having count(*) > 20
+order by postcode_district, loan_count desc;
+
+-- select most popular titles by postcode district
+select 
+    distinct on (postcode_district)
+    postcode_district,
+    title, 
+    author, 
+    count(*) as loan_count
+from loans
+where postcode_district is not null
+and postcode_district ~ '^M[0-9]'
+group by postcode_district, title, author
+having count(*) > 100
+order by postcode_district, loan_count desc;
+
+
+-- select count of loans by ward
 select 
     distinct on (l.ward)
     l.ward,
